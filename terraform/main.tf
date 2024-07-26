182cf998cc3d53719b797c7013e76a1faf8a69a3
Xizi Wang <79822665+seasaur@users.noreply.github.com>
2024-07-26 14:15:18 +0930

    dynamodb name update

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,6 @@ terraform {
     key            = "terraform/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "<DYNAMODB_TABLE_NAME>"
+    dynamodb_table = "seasea"
   }
 }
